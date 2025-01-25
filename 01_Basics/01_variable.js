bfd5c8bf3e5b652e32ca08dd6251ba8c9132036c
Lokesh Kumar ARUDRA <103440456+LokeshKumar-Arudra@users.noreply.github.com>
2025-01-25 20:20:11 +0000

    Contains initial set up details & variables notes

---
+++ 01_Basics/01_variable.js
@@ -0,0 +1,20 @@
+const id = 123
+//id = 345  // reassigning not allowed for const
+
+let email = "abc@gmail.com"
+var password = 234567890   
+/*
+
+*/
+company = "OSI"
+let declared  // gives undefined as output
+
+console.log(typeof company);
+
+console.table([id , email , password , company , declared]) // 
+
+email = "123@gmail.com"
+password = 121111111222222
+company = "LTI"
+
+console.table([id , email , password , company , declared]) // 
