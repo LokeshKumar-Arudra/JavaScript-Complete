55e3ccbc66ea0a1e8421078e1f6920d1b3dbf711
Lokesh Kumar ARUDRA <103440456+LokeshKumar-Arudra@users.noreply.github.com>
2025-01-25 21:14:29 +0000

    changed file location

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,81 @@
+let marks = "83"
+
+let valueInNumber = Number(marks) // "Number" is used to convert string to number
+// console.log(typeof valueInNumber); // number
+
+let bool = true
+// console.log(Number(bool)); // 1
+// console.log(typeof Number(bool)) // number
+// "33" => 33
+// "33abc" => NaN not a number
+// true => 1
+// false => 0
+
+// console.log( typeof NaN); // number
+
+//Boolean:
+let name = "Lokesh"
+let boolValue = Boolean(name) // wont change the reference
+// console.log(typeof name); // string
+// console.log(boolValue); // true
+// "" => false
+let abc = -1
+// console.log(Boolean(abc)); // true
+
+
+//String: 
+let num = 18
+let strNum = String(num)
+// console.log(strNum); 18
+// console.log(typeof strNum); string
+
+
+// ************************ OPERATIONS ***************************//
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " Lokesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+
+
+
+
+
+
+
+
+
