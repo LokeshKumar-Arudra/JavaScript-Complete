b42335ba0986310c8e4da397394670d1893ee2da
Lokesh Kumar ARUDRA <103440456+LokeshKumar-Arudra@users.noreply.github.com>
2025-01-25 20:22:27 +0000

    Contains data types examples

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict" // Treats all the code as new version
+
+//alert(3 + 3) // will give error since we are using node js not browser
+
+console.log(1 +
+     8); // This will execute but it is not recommended to use 
+
+let name = "Lokesh" // data type Number
+let age = 26
+let isLogged = false   
+let state = null ;
+let brand; // undefined
+
+console.log(state);
+
+console.log(typeof name);
+
+// number => 2 to power 53 (2^53)
+// bigint
+
+// string => ""
+// boolean => true/false
+// null => standalone value its data type is object
+// undefined => 
+// symbol => unique
+
+//object
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
