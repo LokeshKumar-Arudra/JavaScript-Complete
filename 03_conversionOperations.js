7872c4b58ce6a7f67b6e0089367b0edcf5d91e77
Lokesh Kumar ARUDRA <103440456+LokeshKumar-Arudra@users.noreply.github.com>
2025-01-25 20:26:01 +0000

    created a new file

---
+++ 03_conversionOperations.js
@@ -0,0 +1,35 @@
+let marks = "83"
+
+let valueInNumber = Number(marks) // "Number" is used to convert string to number
+// console.log(typeof valueInNumber); // number
+
+let bool = true
+// console.log(Number(bool)); // 1
+// console.log(typeof Number(bool)) // number
+// "33" => 33
+// "33abc" => NaN not a number
+// true => 1
+// false => 0
+
+// console.log( typeof NaN); // number
+
+//Boolean:
+let name = "Lokesh"
+let boolValue = Boolean(name) // wont change the reference
+// console.log(typeof name); // string
+// console.log(boolValue); // true
+// "" => false
+let abc = -1
+// console.log(Boolean(abc)); // true
+
+
+//String: 
+
+
+
+
+
+
+
+
+
