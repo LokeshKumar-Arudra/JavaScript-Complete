f4671164cf193f05fb186f6fa7614a569fabcc8b
Lokesh Kumar ARUDRA <103440456+LokeshKumar-Arudra@users.noreply.github.com>
2025-01-28 13:40:59 +0000

    contains string & methods

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,18 @@
+let name = "Lokesh"
+let course = new String ("Js by Hitesh");
+console.log(course.toLowerCase);
+console.log(`Hello ${name}`);
+console.log(course.length);
+console.log(course.charAt(3)); 
+console.log(course.toLowerCase());
+console.log(course.indexOf(" ")); // Gives the very first occurance
+
+console.log(`My name is ${name}`);
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('sundar'))
+
+console.log(gameName.split('-'));
